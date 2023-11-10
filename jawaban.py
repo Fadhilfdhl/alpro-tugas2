607703de28c3bc48b76227aca2788676b55001f9
Fadhil <147614811+Fadhilfdhl@users.noreply.github.com>
2023-11-10 21:12:56 +0700

    jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,35 @@
+# Tipe data integer (int)
+angka_bulat = 10
+
+# Tipe data float (float)
+angka_desimal = 3.14
+
+# Tipe data string (str)
+teks = "HalloWorld"
+
+# Tipe data boolean (bool)
+benar = True
+salah = False
+
+# Tipe data list
+daftar_angka = [1, 2, 3, 4, 5]
+
+# Tipe data tuple
+tuple_angka = (1, 2, 3, 4, 5)
+
+# Tipe data dictionary
+data_mahasiswa = {'nama': 'Fadhil', 'usia': 18, 'kota': 'Bogor'}
+
+# Tipe data set
+set_angka = {1, 2, 3, 4, 5}
+
+# Menampilkan nilai dari setiap tipe data
+print("Nilai dari angka_bulat:", angka_bulat)
+print("Nilai dari angka_desimal:", angka_desimal)
+print("Nilai dari teks:", teks)
+print("Nilai dari benar:", benar)
+print("Nilai dari salah:", salah)
+print("Nilai dari daftar_angka:", daftar_angka)
+print("Nilai dari tuple_angka:", tuple_angka)
+print("Nilai dari data_mahasiswa:", data_mahasiswa)
+print("Nilai dari set_angka:", set_angka)
