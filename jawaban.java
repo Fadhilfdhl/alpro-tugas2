f917cf9b9e9ae784e5bee9bb59911e9b0678d0ec
Fadhil <147614811+Fadhilfdhl@users.noreply.github.com>
2023-11-10 20:57:38 +0700

    jawaban.java

---
+++ jawaban.java
@@ -0,0 +1,27 @@
+class TipeDataJava {
+    public static void main(String[] args) {
+        // Tipe data numerik
+        byte byteNilai = 127;
+        short shortNilai = 32767;
+        int intNilai = 2147483647;
+        long longNilai = 9223372036854775807L;
+        float floatNilai = 3.14f;
+        double doubleNilai = 2.71828;
+
+        // Tipe data karakter
+        char charNilai = 'A';
+
+        // Tipe data boolean
+        boolean booleanNilai = true;
+
+        // Menampilkan nilai dari setiap tipe data
+        System.out.println("Nilai byte: " + byteNilai);
+        System.out.println("Nilai short: " + shortNilai);
+        System.out.println("Nilai int: " + intNilai);
+        System.out.println("Nilai long: " + longNilai);
+        System.out.println("Nilai float: " + floatNilai);
+        System.out.println("Nilai double: " + doubleNilai);
+        System.out.println("Nilai char: " + charNilai);
+        System.out.println("Nilai boolean: " + booleanNilai);
+    }
+}
